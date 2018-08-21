fd7fefab92254c6ad57f9c99d1b01d6efd4b9db5
agomezomg <anagomez98@unitec.edu>
2018-08-20 22:20:03 -0600

    50 aulas creadas

---
+++ proyectoLdPprolog.pl
@@ -0,0 +1,79 @@
+aula(nh101, 50, 10).
+aula(nh201, 50, 10).
+aula(nh301, 50, 10).
+aula(m2001, 30, 5).
+aula(etaa2, 30, 5).
+aula(etb101, 20, 5).
+aula(hkd101, 80, 20).
+aula(v4nja, 69, 5).
+aula(zmh3k, 16, 10).
+aula(9pjqq, 19, 15).
+aula(ci19y, 98, 20).
+aula(k3vzu, 10, 25).
+aula(35jl2, 53, 30).
+aula(uw4tp, 58, 35).
+aula(zo0jj, 68, 40).
+aula(vdrn1, 55, 45).
+aula(iqr46, 32, 50).
+aula(5mufe, 37, 55).
+aula(7a64n, 43, 60).	
+aula(mdxfp, 56, 65).
+aula(zhnvt, 11, 70).
+aula(5mo47, 27, 75).
+aula(xd82w, 45, 80).
+aula(6vmq9, 65, 85).
+aula(leyjv, 26, 90).
+aula(np490, 28, 95).
+aula(qx2o9, 14, 25).
+aula(1uuci, 41, 15).
+aula(blio6, 34, 30).
+aula(v9fd6, 26, 95).
+aula(xebno, 99, 90).
+aula(3m1sz, 97, 75).
+aula(ez0ih, 57, 85).
+aula(5415i, 22, 65).
+aula(hmtkq, 55, 60).
+aula(4abcq, 59, 45).
+aula(pehdk, 74, 5).
+aula(c02wg, 66, 20).
+aula(rhtql, 78, 10).
+aula(ek2l3, 46, 40).
+aula(enkhh, 70, 70).
+aula(enu4x, 74, 50).
+aula(k2ks8, 60, 55).
+aula(sahaa, 40, 80).
+aula(xhugd, 89, 35).
+aula(d79bj, 42, 10).
+aula(yzpba, 32, 95).
+aula(0piis, 23, 75).
+aula(nv34v, 35, 20).
+aula(sq4tn, 64, 65).
+aula(i0jmu, 41, 80).
+aula(cahva, 43, 35).
+aula(rn21g, 10, 15).
+aula(z9l9c, 20, 60).
+aula(k2sc9, 30, 45).
+aula(m4bcw, 40, 90).
+aula(jn6wd, 50, 25).
+
+
+estudiante(2000100001, 1, [math101, phys201, ec201]) .
+estudiante(2000100002, 1, [math101, phys201, hist301]).
+estudiante(2000100003, 1, [math101, ec201, hist301]) .
+estudiante(2000100004, 1, [math101, phys201, ec201, hist301]) .
+estudiante(2000100007, 0, [ec201, phys201]).
+estudiante(2000100015, 0, [ec201, hist301]) .
+estudiante(2000100016, 0, [cmpe150, hist301]).
+estudiante(2000100017, 0, []).
+estudiante(2000100008, 0, [math101, phys201]) .
+estudiante(2000100009, 1, [phys201, ec201]).
+estudiante(2000100010, 0, [cmpe150, math101]).
+estudiante(2000100011, 0, [cmpe150, hist301]).
+estudiante(2000100012, 1, [math101, phys201]).
+estudiante(2002100009, 0, [math101, ec201, hist301]).
+estudiante(2002100071, 0, [phys201, ec201, hist301]).
+estudiante(2002100053, 0, [cmpe150, ec201]).
+estudiante(2002100040, 1, [math101, hist301]).
+estudiante(2002100032, 0, [phys201, ec201]).
+estudiante(2002100010, 0, [cmpe150]).
+estudiante(2002103010, 1, []).
